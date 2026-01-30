1266a5c0396d227868edc9256e25ba7004952df1
teja-goud-kandula <kandulateja6@gmail.com>
2026-01-30 22:42:27 +0530

    Sum of numbers in a range

---
+++ Day5/range.py
@@ -0,0 +1,15 @@
+empty_list = []
+for i in range(10):
+    empty_list.append(i)
+
+print(empty_list)
+# Output is:
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# Finding sum of numbers in a range
+sum_total = 0
+for i in range(1, 11):  # from 1 to 10 inclusive
+    sum_total += i
+
+print(f"Sum from 1 to 10 is: {sum_total}")
